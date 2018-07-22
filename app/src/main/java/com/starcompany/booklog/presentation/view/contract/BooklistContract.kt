62d3feb71da2734bdb08eedcb5f8f1ce44d14f12
yoshida0261 <yoshida261@gmail.com>
2018-07-23 01:21:55 +0900

    refactor file path

---
+++ app/src/main/java/com/starcompany/booklog/presentation/view/contract/BooklistContract.kt
@@ -0,0 +1,10 @@
+package com.starcompany.booklog.presentation.view.contract
+
+import com.starcompany.booklog.presentation.view.BaseView
+
+interface BooklistContract{
+    interface View: BaseView{
+
+    }
+
+}
